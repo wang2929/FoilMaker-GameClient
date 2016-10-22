2e19c5a5782590ba675506af27c0b7dcd1c37ae4
wang2929 <wang2929@purdue.edu>
2016-10-22 10:41:43 -0400

    Update foilmakerModel.java

--- foilmakerModel.java
+++ foilmakerModel.java
@@ -2,6 +2,8 @@
  * Created by Yufei Xu on 10/22/2016.
  * captures data (questions, word suggestions, etc. received from the	server)
  */
+
+//Something here - Tiffany
 public class foilmakerModel {
 
 }
